6b186daa342699b9dcb6c1296d4cf42b7ccf8fc9
Eline Schaft <eline@stein.umcutrecht.nl>
2020-08-19 10:41:21 +0200

    changed example for my own folder directions

--- example/run_annotatedTRC2bids.m
+++ example/run_annotatedTRC2bids.m
@@ -3,9 +3,9 @@ addpath('../trc2bids/')
 addpath('../micromed_utils/')
 addpath('../external/')
 
-fieldtrip_folder  = '/home/matteo/Desktop/git_rep/fieldtrip/';
-fieldtrip_private = '/home/matteo/Desktop/git_rep/fieldtrip/fieldtrip_private/';
-jsonlab_folder    = '/home/matteo/Desktop/git_rep/jsonlab/';
+fieldtrip_folder  = '/home/eline/Documents/Git/Utils/fieldtrip/';
+fieldtrip_private = '/home/eline/Documents/Git/Utils/fieldtrip/fieldtrip_private/';
+jsonlab_folder    = '/home/eline/Documents/Git/Utils/jsonlab/';
 addpath(fieldtrip_folder) 
 addpath(fieldtrip_private)
 addpath(jsonlab_folder)
@@ -13,8 +13,8 @@ addpath(jsonlab_folder)
 
 
 cfg          = [];
-cfg.proj_dir = '/home/matteo/Desktop/tmp/';            % folder to store bids files
-cfg.filename = '/home/matteo/Desktop/tmp/example.TRC'; % TRC file
+cfg.proj_dir = '/home/eline/Documents/Data/aECoG_temp/';            % folder to store bids files
+cfg.filename = '/home/eline/Documents/Data/aECoG_temp/example1.TRC'; % TRC file
 
 
 [status,msg,output] = annotatedTRC2bids(cfg)
